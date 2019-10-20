09813700749d18e9c7353ecdfca994d7e9db572f
abdallahokasha <okasha.fci@gmail.com>
2019-10-20 16:33:33 +0200

    Add sequelize authenticate to index.

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const db = require("./models");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 let port = process.env.PORT || 1234;
 
-app.listen(port, () => {
-  console.log("[index.js] Server is up and running on port:", port);
+db.sequelize.authenticate().then(() => {
+  app.listen(port, () => {
+    console.log("[index.js] Server is up and running on port:", port);
+  });
 });
